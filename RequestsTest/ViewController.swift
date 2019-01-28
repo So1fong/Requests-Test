222792a4e28e6a16676c50b06df4217f18289303
Ekaterina <zireael6@gmail.com>
2019-01-28 19:41:50 +0300

    создан первый запрос к серверу с началом сессии

--- RequestsTest/ViewController.swift
+++ RequestsTest/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController
+{
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        createNewSession()
     }
+    
+    func createNewSession()
+    {
+        guard let url = URL(string: "https://bnet.i-partner.ru/testAPI/") else { return }
+        let parameters = ["a":"new_session"]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("token", forHTTPHeaderField: "cZl7kQI-8H-H4HQhWc")
+        //request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error)  in
+            if let response = response
+            {
+                print(response)
+            }
+            guard let data = data else { return }
+            do
+            {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            }
+            catch
+            {
+                print(error)
+            }
+        }.resume()
+    }
 
 }
 
