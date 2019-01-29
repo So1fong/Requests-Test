5bad3c45f14b0883d1f0479cf33c5c9b6694940d
Ekaterina <zireael6@gmail.com>
2019-01-29 15:46:59 +0300

    добавлен запрос на отображение списка записей пользователя

--- RequestsTest/ViewController.swift
+++ RequestsTest/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
+struct newSessionAnswer: Codable
+{
+    var data: dataStruct
+    var status: Int
+}
+
+struct dataStruct: Codable
+{
+    var session: String
+}
+
+var sessionId: String = ""
 
 class ViewController: UIViewController
 {
-    override func viewDidLoad() {
+
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         createNewSession()
+        showUserList()
     }
     
     func createNewSession()
@@ -33,12 +50,44 @@ class ViewController: UIViewController
             {
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
                 print(json)
+                let answer = try JSONDecoder().decode(newSessionAnswer.self, from: data)
+                print("Parsed:")
+                print(answer)
+                let str: String = answer.data.session
+                sessionId = str
             }
             catch
             {
                 print(error)
             }
         }.resume()
+    }
+    
+    func showUserList()
+    {
+        guard let url = URL(string: "https://bnet.i-partner.ru/testAPI/") else { return }
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let requestStr = "a=get_entries&session=" + sessionId
+        request.httpBody = requestStr.data(using: .utf8)
+        request.addValue("cZl7kQI-8H-H4HQhWc", forHTTPHeaderField: "token")
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        session.dataTask(with: request) { (data, response, error)  in
+            guard let data = data else { return }
+            do
+            {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+                //let answer = try JSONDecoder().decode(newSessionAnswer.self, from: data)
+                //print("Parsed:")
+                //print(answer)
+            }
+            catch
+            {
+                print(error)
+            }
+            }.resume()
     }
 
 }
